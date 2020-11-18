ff65c35251e6eb90a5e66cff8658f2abcf230c32
Christos Mathas <mathas.ch.m@gmail.com>
2020-11-18 12:43:27 +0200

    Fixed JWT not checking signature vulnerability. Added OWASP dependency checker in pom.xml for Vulnerable Components task.

--- webgoat-lessons/jwt/src/main/java/org/owasp/webgoat/plugin/JWTVotesEndpoint.java
+++ webgoat-lessons/jwt/src/main/java/org/owasp/webgoat/plugin/JWTVotesEndpoint.java
@@ -139,7 +139,7 @@ public class JWTVotesEndpoint extends AssignmentEndpoint {
             return trackProgress(failed().feedback("jwt-invalid-token").build());
         } else {
             try {
-                Jwt jwt = Jwts.parser().setSigningKey(JWT_PASSWORD).parse(accessToken);
+                Jwt jwt = Jwts.parser().setSigningKey(JWT_PASSWORD).parseClaimsJws(accessToken);
                 Claims claims = (Claims) jwt.getBody();
                 boolean isAdmin = Boolean.valueOf((String) claims.get("admin"));
                 if (!isAdmin) {
